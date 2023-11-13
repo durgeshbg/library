e8475e46fd9632ad4249b9a29ab80bdaf17213fc
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2023-11-13 08:57:58 +0530

    add Book constructor

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log('Testing');
+const myLibrary = [];
+
+function Book(title, author, pages, hasRead) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.hasRead = hasRead ? 'read' : 'not read yet';
+    this.info = function () {
+        return `${this.title} by ${this.author}, ${this.pages} pages, has ${this.hasRead}.`;
+    };
+}
+
+function addBookToLibrary() {
+    // do stuff here
+}
