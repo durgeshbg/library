4b11c11cb3e94043b89b0b8cee71ec36acc8380e
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2023-11-14 16:23:52 +0530

    add delete funtionality for a book card

--- script.js
+++ script.js
@@ -19,37 +19,7 @@ function addBookToLibrary(title, author, pages, hasRead) {
     myLibrary.push(book);
 }
 
-function logBooks() {
-    const container = document.querySelector('.container');
-    container.innerHTML = '';
-    myLibrary.forEach((book) => {
-        const card = document.createElement('div');
-        card.classList.add('card');
-        for (const key in book) {
-            if (key == 'hasRead') {
-                let content = book[key] ? 'not read yet' : 'read';
-                card.innerHTML += `<button class=${key}>has ${content}</button>`;
-                card.lastElementChild.addEventListener('click', (e) => {
-                    card.classList.toggle('read');
-                    book[key] = !book[key];
-                    e.target.textContent = book[key] ? 'not read yet' : 'read';
-                });
-                if (book[key]) card.classList.toggle('read');
-            } else card.innerHTML += `<div class="${key}">${book[key]}</div>`;
-        }
-        container.appendChild(card);
-    });
-}
-
-const dialog = document.querySelector('dialog');
-document
-    .querySelector('.add')
-    .addEventListener('click', () => dialog.showModal());
-document
-    .querySelector('.close')
-    .addEventListener('click', () => dialog.close());
-
-document.querySelector('form').addEventListener('submit', (e) => {
+function handleSubmit(e) {
     const formData = new FormData(e.target);
     const book = {};
     formData.forEach((value, key) => {
@@ -66,6 +36,43 @@ document.querySelector('form').addEventListener('submit', (e) => {
     dialog.close();
     logBooks();
     e.preventDefault();
-});
+}
 
+function deleteBook(e) {
+    e.target.parentElement.remove();
+}
+
+function logBooks() {
+    const container = document.querySelector('.container');
+    container.innerHTML = '';
+    myLibrary.forEach((book) => {
+        const card = document.createElement('div');
+        card.classList.add('card');
+        card.innerHTML = "<button class='delete'>&#x1F5D1;</button>";
+        for (const key in book) {
+            if (key == 'hasRead') {
+                let content = book[key] ? 'not read yet' : 'read';
+                card.innerHTML += `<button class=${key}>has ${content}</button>`;
+                card.lastElementChild.addEventListener('click', (e) => {
+                    card.classList.toggle('read');
+                    book[key] = !book[key];
+                    e.target.textContent = book[key] ? 'not read yet' : 'read';
+                });
+                if (book[key]) card.classList.toggle('read');
+            } else card.innerHTML += `<div class="${key}">${book[key]}</div>`;
+        }
+        card.firstChild.addEventListener('click', deleteBook);
+        container.appendChild(card);
+    });
+}
+
+const dialog = document.querySelector('dialog');
+document
+    .querySelector('.add')
+    .addEventListener('click', () => dialog.showModal());
+document
+    .querySelector('.close')
+    .addEventListener('click', () => dialog.close());
+
+document.querySelector('form').addEventListener('submit', handleSubmit);
 logBooks();
